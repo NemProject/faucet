ffb5ea2a4bcdf90d0b36d3c76a1a4b6916442dcc
AnthonyLaw <yc-law1015@hotmail.com>
2022-01-19 01:05:05 +0800

    feat: add new services

---
+++ src/services/fetch.js
@@ -0,0 +1,85 @@
+/*
+ * (C) Symbol Contributors 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ * POST method
+ * @param {string} url target url.
+ * @param {object} body json body.
+ * @param  {...any} config Fetch config.
+ * @returns {data} resolved json data.
+ */
+export const FetchPost = async (url, body, ...config) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+        ...config
+    });
+
+    if (!response.ok) {
+        const responseText = await response.text();
+        const msg = JSON.parse(responseText)
+
+        return {
+            data: null,
+            error: {
+                url: response.url,
+                message: msg.error,
+                code: response.status,
+            }
+        }
+    }
+
+    return {
+        data: await response.json(),
+    }
+}
+
+/**
+ * GET method
+ * @param {string} url target url.
+ * @param  {...config} config Fetch config.
+ * @returns {data} resolved json data.
+ */
+export const FetchGet = async (url, ...config) => {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        ...config
+    });
+
+    if (!response.ok) {
+        const responseText = await response.text();
+        const msg = JSON.parse(responseText)
+
+        return {
+            data: null,
+            error: {
+                url: response.url,
+                message: msg.error,
+                code: response.status,
+            }
+        }
+    }
+
+    return {
+        data: await response.json(),
+    }
+}
---
+++ src/services/nemRequest.js
@@ -0,0 +1,71 @@
+/*
+ * (C) Symbol Contributors 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+import {FetchGet, FetchPost} from './fetch';
+
+const endpoint = 'http://hugetestalice.nem.ninja:7890';
+
+/**
+ * Gets account balance.
+ * @param {string} address - Account address.
+ * @returns {Object} Balance - Account balance.
+ */
+export const getBalance = async (address) => {
+    const { data } = await FetchGet(`${endpoint}/account/get?address=${address}`);
+
+    if (!data) {
+        throw new Error(`Can't get address balance ${address}`);
+    }
+
+    return {
+       address: data.account.address,
+       balance: data.account.balance,
+    }
+}
+
+/**
+ * Announce payload to the network.
+ * @param {string} payload - signed transaction payload.
+ * @returns {Object} announce transaction status.
+ */
+export const announceTransaction = async (payload) => {
+    const { data } = await FetchPost(`${endpoint}/transaction/announce`, payload)
+
+    if (!data) {
+        throw new Error(`Can't announce transaction: ${payload}`);
+    }
+
+    return {
+        message: data.message,
+        transactionHash: data.transactionHash.data,
+    };
+}
+
+/**
+ * Gets timestamp from network.
+ * @returns {Object} timestamp.
+ */
+export const getNetworkTime = async () => {
+    const { data } = await FetchGet(`${endpoint}/time-sync/network-time`)
+
+    if (!data) {
+        throw new Error(`Can't get network time.`);
+    }
+
+    return {
+        timestamp: data.sendTimeStamp,
+    };
+}

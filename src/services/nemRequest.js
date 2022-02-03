65794ff87b93d160dca50286e5a8da0420c47a0d
AnthonyLaw <yc-law1015@hotmail.com>
2022-02-03 16:25:17 +0800

    task: add endpoint nem request service

---
+++ src/services/nemRequest.js
@@ -0,0 +1,43 @@
+/*
+ * (C) Symbol Contributors 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+import axios from 'axios';
+
+const endpoint = 'http://hugetestalice.nem.ninja:7890';
+
+const client = axios.create({
+	baseURL: endpoint
+});
+
+const NemRequest = {
+/**
+ * Gets account balance.
+ * @param {string} _address - Account address.
+ * @returns {object} Balance - Account balance.
+ */
+	getAccountInfo: async _address => {
+		try {
+			const response = await client.get(`/account/get?address=${_address}`);
+
+			return { response };
+		} catch (error) {
+			console.error(`Can't get address balance ${_address}`);
+			return { error };
+		}
+	}
+};
+
+export default NemRequest;
